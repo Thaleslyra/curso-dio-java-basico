cf6a6a27071952f8c860c0a364fc150b830b38c8
Thales <thalespipa@hotmail.com>
2024-05-05 15:47:43 -0300

    adicionando pasta idea ao .gitignore

--- src/edu/thales/primeirasemana/MinhaClasse.java
+++ src/edu/thales/primeirasemana/MinhaClasse.java
@@ -1,7 +1,10 @@
+package edu.thales.primeirasemana;
+
 public class MinhaClasse {
     public static void main(String[] args) {
 
         System.out.println(nomeCompleto("Thales", "André"));
+        Integer primeiroNumero = 34;
 
     }
 
