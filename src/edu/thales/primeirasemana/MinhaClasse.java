b1798d0ef661739752401dd9eeb0cd8bb496d93a
Thales <thalespipa@hotmail.com>
2024-06-12 14:50:48 -0300

    aula métodos e excopos: adicionado o pacote sistema_smart_tv ao projeto

--- src/edu/thales/primeirasemana/MinhaClasse.java
+++ src/edu/thales/primeirasemana/MinhaClasse.java
@@ -1,10 +1,19 @@
 package edu.thales.primeirasemana;
 
+
 public class MinhaClasse {
     public static void main(String[] args) {
 
         System.out.println(nomeCompleto("Thales", "André"));
         Integer primeiroNumero = 34;
+
+        String nome = "GLEYSON";
+        int idade = 22;
+        double peso = 68.5;
+        char sexo = 'M';
+        boolean doadorOrgao = false;
+
+
 
     }
 
